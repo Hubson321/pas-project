e6ff855ecc9105547e032d5b21f930e05149b525
Hubert <hubson321@gmail.com>
2024-06-10 13:09:04 +0200

    x

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,44 +1,33 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from "react-router-dom"; 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
 
 import Upload from "./components/Upload";
 import Results from "./components/Results";
 import GetCounter from "./components/GetCounter";
-import ImageDetail from "./components/ImageDetail";
 
 import "./App.css";
 
 const App = () => {
-  const [refreshResults, setRefreshResults] = useState(false);
-  const navigate = useNavigate();
-
-  const handleLinkClick = () => {
-    setRefreshResults(true);
-    // Navigate to the route without refreshing the page
-    navigate(window.location.pathname);
-  };
-
   return (
     <Router>
       <div>
         <nav className="navbar">
           <ul className="nav-links">
             <li>
-              <Link to="/upload" onClick={handleLinkClick}>Upload</Link>
+              <Link to="/upload">Upload</Link>
             </li>
             <li>
-              <Link to="/results" onClick={handleLinkClick}>Results</Link>
+              <Link to="/results">Results</Link>
             </li>
             <li>
-              <Link to="/get-counter" onClick={handleLinkClick}>Get Counter</Link>
+              <Link to="/get-counter">Get Counter</Link>
             </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/upload" element={<Upload />} />
-          <Route path="/results" element={<Results refresh={refreshResults} setRefresh={setRefreshResults} />} />
+          <Route path="/results" element={<Results />} />
           <Route path="/get-counter" element={<GetCounter />} />
-          <Route path="/image/:imageId" element={<ImageDetail />} /> {/* Add route for ImageDetail */}
           <Route path="/" element={<h2>Welcome to Azure Image Analyzer</h2>} />
         </Routes>
       </div>
